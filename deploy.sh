d807bac856ebfb30e9879a1df59f2ba99d218874
kdaflaoui <daflaoui.karim@yahoo.com>
2020-10-05 21:07:15 -0400

    add the header in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+##############################################################
+#
+#	Description: deploiment un conteneur docker
+#
+#
+#	Auteur : Karim
+#
+##############################################################
+
+echo "
+
+Options : 
+	- -- Create : launch the caontainers
+	- -- Delete : the containers created by deploy.sh
+	- -- Infos : features of the  containers
+	- -- Start : restart the containers
+	- -- Ansible : doploy the ansible arborescence 
+"
+
+
