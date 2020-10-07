0f99eab4b7fafaaae9b25b1cdc94fa32a38b21c2
kdaflaoui <daflaoui.karim@yahoo.com>
2020-10-06 21:19:02 -0400

    add diffrents options

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,30 @@
 #
 ##############################################################
 
-echo "
 
-Options : 
-	- -- Create : launch the caontainers
-	- -- Delete : the containers created by deploy.sh
-	- -- Infos : features of the  containers
-	- -- Start : restart the containers
-	- -- Ansible : doploy the ansible arborescence 
+if  [ $1 == "--Create" ]; then
+	echo "Create option..."
+
+elif [ $1 == "--Drop" ]; then
+	echo "Delete option..."
+
+elif [ $1 == "--Info" ]; then
+	echo "Info option..."
+
+elif [ $1 == "--Start" ]; then
+	echo "Restar option"
+
+elif [ $1 == "--Ansible" ]; then
+	echo "Ansible option..."
+
+else
+	echo "
+
+		Options : 
+			- -- Create : launch the caontainers
+			- -- Drop : the containers created by deploy.sh
+			- -- Infos : features of the  containers
+			- -- Start : restart the containers
+			- -- Ansible : doploy the ansible arborescence 
 "
-
-
+fi
