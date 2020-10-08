4c537fc6610a7a0358c8a063225d1c63b85cb0d2
kdaflaoui <daflaoui.karim@yahoo.com>
2020-10-08 18:42:59 -0400

    create and delete a containers

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,33 @@
 
 
 if  [ $1 == "--Create" ]; then
+	
 	echo "Create option..."
+	
+	nb_machine=1
+	
+	[ $2 != "" ] && nb_machine=$2
+	
+	for i in $(seq 1 $nb_machine); do
+	
+		docker run -tid --name $USER-alpine-$i alpine:latest 
+		echo "The container $USER-alpine-$i created"	
+	done
+	
+	echo "Number of instance created is : $2 "	
 
 elif [ $1 == "--Drop" ]; then
 	echo "Delete option..."
+	docker rm -f $(docker ps | grep $USER-alpine | awk '{ print $1 }')
 
 elif [ $1 == "--Info" ]; then
 	echo "Info option..."
 
 elif [ $1 == "--Start" ]; then
 	echo "Restar option"
+	for i in $(seq 1 10); do
+		echo $i
+	done
 
 elif [ $1 == "--Ansible" ]; then
 	echo "Ansible option..."
